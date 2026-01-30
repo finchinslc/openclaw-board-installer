ae7956a145ff0bf41461433f0437a028d2ff7802
Finch <finchinslc@gmail.com>
2026-01-30 16:04:14 -0700

    chore: add .gitignore, remove node_modules from tracking

--- dist/cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { runInstaller } from './installer.js';
-runInstaller().catch((err) => {
-    console.error('Installation failed:', err.message);
-    process.exit(1);
-});
--- dist/installer.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function runInstaller(): Promise<void>;
